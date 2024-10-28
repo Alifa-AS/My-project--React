c2a89a71edca706df17736fda056736fbcb96837
AlifaSrn <alifa.nub@gmail.com>
2024-10-28 17:54:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DaisyNav from './Components/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+    <DaisyNav></DaisyNav>
+      <h1 className='text-5xl'>Vite + React</h1>
+    {/* hero */}
+  
+    </>
+  )
+}
+
+export default App
